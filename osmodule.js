1b8789067d04791f1bc156a44eb2b4b0152fcbd7
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-07-30 23:20:25 +0530

    osmodul run

---
+++ osmodule.js
@@ -0,0 +1,5 @@
+const os = require('node:os');
+
+console.log(os.freemem())
+console.log(os.hostname())
+console.log(os.homedir())
--- second.js
+++ second.js
@@ -1,6 +1,11 @@
+// (function(exports, require, module, __filename, __dirname){
+
+
 sonam = {
     name: "Sonam",
     FavNum: 36,
     developer:true
 }
+console.log(exports, require, module, __filename, __dirname)
 module.exports = sonam;
+// })
