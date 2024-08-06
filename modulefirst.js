e244d1722f2e45cd18dfeafde8732aafc72f42bc
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-08-06 22:16:58 +0530

    urlmodule.js running

---
+++ modulefirst.js
@@ -0,0 +1,4 @@
+// import {simple, simple2} from "./modulesecond.mjs"
+import * as a2 from "./modulesecond.mjs"
+// simple2()
+console.log(a2.simple())
---
+++ urlmodule.js
@@ -0,0 +1,5 @@
+import url from 'url';
+
+import url from 'node:url';
+const myURL =
+  url.parse('https://user:pass@sub.example.com:8080/p/a/t/h?query=string#hash');
